187b1740acfec94b954f0a1c0d9efbe2e068d34f
wiersinga <wiersingaolfa@yahoo.fr>
2023-10-02 16:54:30 +0200

    create Header, reservation-container

---
+++ vue-project/src/components/CustomHeader.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="headerBox">
+    <div class="headerLeft">
+      <img width="120" height="120" src="./icons/bird-logo.jpg">
+      <a href="#">Louer</a>
+      <a href="#">Abonnement</a>
+    </div>
+    <div class="headerRight">
+      <a href="#"><img src="./icons/langue-icone.png"></a>
+      <a href="#">Inscription/Connexion</a>
+    </div>
+  </div>
+  <div class="imgHeader">
+<!--    <img src="./icons/carO.jpg">-->
+    <CustomReservation />
+  </div>
+</template>
+<!--     <h1>Header Page</h1>-->
+
+<script>
+import CustomReservation from "@/components/CustomReservation.vue";
+
+export default {
+  name: 'CustomHeader',
+  components: {CustomReservation},
+  // components : {
+  //   CustomHeader
+  // }
+}
+</script>
+
+<style scoped>
+.headerBox {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #ffffff;
+  padding: 10px 20px;
+  color: black;
+  height: 80px;
+  border-radius: 5px;
+}
+.headerLeft {
+  display: flex;
+  align-items: center;
+}
+.headerRight {
+  display: flex;
+  align-items: center;
+}
+.headerLeft a,
+.headerRight a {
+  margin-left: 20px;
+  text-decoration: none;
+  color: black;
+  font-family: system-ui;
+  font-size: 25px;
+  font-weight: bold;
+}
+.headerLeft a:hover,
+.headerRight a:hover {
+  //text-decoration: underline;
+  color: blueviolet;
+}
+
+
+</style>
---
+++ vue-project/src/components/CustomReservation.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="containerReser">
+
+  <div class="typeVehicle">
+    <div class="car-type">
+      <img src="./icons/voiture.png">
+      <a href="#"> Véhicules</a>
+    </div>
+
+    <div class="moto-type">
+      <img src="./icons/moto.png">
+      <a href="#"> Moto</a>
+    </div>
+
+    <div class="truck-type">
+      <img src="./icons/camion.png">
+      <a href="#"> Utilitaires</a>
+    </div>
+  </div>
+
+  <div class="pickUp-return-date">
+    <div class="datePick">
+      <label for="datePick">Date de départ</label>
+      <input type="date" id="datePick" v-model="selectedPickDate" />
+    </div>
+
+    <div class="dateReturn">
+      <label for="dateReturn">Date de départ</label>
+      <input type="date" id="dateReturn" v-model="selectedReturnDate" />
+    </div>
+    <!--      <p>{{ selectedDate }}</p>-->
+  </div>
+
+</div>
+</template>
+
+<style scoped>
+.containerReser {
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border: 2px solid black;
+  width: 55%;
+  min-height: 150px;
+}
+.typeVehicle {
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  gap: 20px; /* Espace entre les images */
+  padding-bottom: 20px;
+}
+.datePick, .dateReturn {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+.car-type , .moto-type , .truck-type{
+  display: flex;
+  justify-content: center;
+  align-items: center; /* Pour aligner verticalement le contenu */
+  gap: 10px; /* Espacement horizontal entre l'image et le lien */
+  border: 1px black solid;
+  border-radius: 40px;
+  width: 150px;
+  background-color: bisque;
+}
+.pickUp-return-date{
+  display: flex;
+  justify-content: space-around;
+  gap: 50px;
+}
+</style>
+
+
+
+<script>
+export default {
+  data() {
+  return {
+  selectedPickDate: '',
+    selectedReturnDate: ''
+};
+}
+}
+
+</script>
